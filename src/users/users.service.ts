72ba46686ed5d1d5359472c2704e3a81ebc0d0f7
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-09-09 23:41:16 -0300

    feat: create crypt utils

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+}
