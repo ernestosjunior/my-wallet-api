972b8497f9320d6fb8a0baf8df908a8525993e16
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-09-10 00:31:39 -0300

    feat: implement jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private usersService: UsersService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.SECRET,
+    });
+  }
+
+  async validate(payload: { sub: User['id']; name: string }) {
+    const user = this.usersService.getUserById(payload.sub);
+
+    if (!user) {
+      throw new UnauthorizedException('Unauthorized');
+    }
+
+    return user;
+  }
+}
